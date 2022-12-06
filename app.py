6973f964168ffd22320dbdefb2d7721514604ab8
Hao Nguyen <nguyenquanghao1808@gmail.com>
2022-12-07 04:35:06 +0700

    Challenge1-ObjectDetect

--- app.py
+++ app.py
@@ -6,8 +6,10 @@ import tensorflow as tf
 
 app = Flask(__name__)
 
-model = tf.keras.models.load_model('./model/mnist.h5')
+model = tf.keras.models.load_model('Web\Hand-digit-recoginition-flask\model\keras.h5')
 model.make_predict_function()
+
+
 
 @app.route('/')
 def index():
@@ -15,6 +17,7 @@ def index():
     return render_template('index.html')
 
 @app.route('/recognize', methods =['POST'])
+
 def recognize():
     
     if request.method == 'POST':
@@ -26,24 +29,28 @@ def recognize():
         with open("temp.jpg", "wb") as temp:
             temp.write(imgBytes)
             
+        with open("Web\Hand-digit-recoginition-flask\class_names.txt") as f:
+            classes = f.readlines()
+        classes = [c.replace('\n','').replace(' ','_') for c in classes]
+
         image = cv2.imread('temp.jpg')
-        image = cv2.resize(image, (28, 28), interpolation=cv2.INTER_AREA)
+        image = cv2.resize(image, (28,28), interpolation=cv2.INTER_AREA)
         image_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        image_pred = image_gray.reshape(1, 28, 28, 1)
-        image_pred = (255 - image_pred.astype('float')) / 255
-        
-        
-        prediction = np.argmax(model.predict(image_pred), axis = -1)
-        
+
+        image_prediction = np.reshape(image_gray, (28,28,1))
+        image_prediction = (255 - image_prediction.astype('float')) / 255
+
+
+        prediction = model.predict(np.expand_dims(image_prediction, axis=0))[0]
+        prediction = classes[np.argmax(prediction, axis=0)]
 
         #run prediction
 
         return jsonify({
-            'prediction': str(prediction[0]),
+            'prediction': str(prediction),
             'status': True
         })
         
         
 if __name__ == '__main__':
     app.run(debug=True)
-
